f3f1aec9fd62d32217dcb9093d1a80710df7da44
Abhijeet Das <mns102720@gmail.com>
2026-08-27 20:58:24 +0530

    feat: implement job dispatcher

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 require (
 	filippo.io/edwards25519 v1.2.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/confluentinc/confluent-kafka-go/v2 v2.15.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/go-sql-driver/mysql v1.10.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
---
+++ services/worker-service/internal/platform/dispatcher/dispatcher.go
@@ -0,0 +1,32 @@
+package Dispatcher
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/AbhijeetDev102/Nimbus/services/worker-service/internal/domain"
+	"github.com/AbhijeetDev102/Nimbus/shared/types"
+)
+
+type Dispatcher struct {
+	handlers map[types.JobType]domain.JobHandler
+}
+
+func NewDispatcher() *Dispatcher {
+	return &Dispatcher{
+		handlers: make(map[types.JobType]domain.JobHandler),
+	}
+}
+
+func (d *Dispatcher) Register(jobType types.JobType, handler domain.JobHandler) {
+	d.handlers[jobType] = handler
+}
+
+func (d *Dispatcher) Dispatch(ctx context.Context, job *domain.Job) (*domain.ExecutionResult, error) {
+	value, ok := d.handlers[job.JobType]
+	if !ok {
+		return nil, fmt.Errorf("unsupported job type: %s", job.JobType)
+	}
+
+	return value.Execute(ctx, job)
+}
